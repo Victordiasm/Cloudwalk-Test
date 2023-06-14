7b508b4bfa5f1d4fff43c5c720eb041890c4a3ca
Victor Dias <Victor.diasm@gmail.com>
2023-06-14 18:12:34 -0300

    Improve message in case of no argument.

--- report.py
+++ report.py
@@ -29,5 +29,5 @@ if __name__ == "__main__":
     if len(sys.argv) == 2:
         main()
     else:
-        print("report.py uses an argument of the log path. Please include one.")
+        print("report.py uses an argument of the log path. Please include one. Example: py report.py .\db\qgames.log")
     
